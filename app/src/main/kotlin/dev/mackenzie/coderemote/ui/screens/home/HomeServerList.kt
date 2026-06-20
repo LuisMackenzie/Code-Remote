466cf543d6dff5b19abad4aa415151eb6422b0f3
mackenzie <principemackenzie@gmail.com>
2026-06-20 20:32:15 +0200

    fix import

--- app/src/main/kotlin/dev/mackenzie/coderemote/ui/screens/home/HomeServerList.kt
+++ app/src/main/kotlin/dev/mackenzie/coderemote/ui/screens/home/HomeServerList.kt
@@ -3,7 +3,6 @@ package dev.mackenzie.coderemote.ui.screens.home
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillParentMaxHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
